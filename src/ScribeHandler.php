d4c0f705794c08025d4b7b51b3765f371bfa38c6
Plenty Su <plentysu@kkbox.com>
2014-12-06 01:53:59 +0800

    Impelemnt ScribeHandler

--- src/ScribeHandler.php
+++ src/ScribeHandler.php
@@ -4,11 +4,20 @@ namespace Iconvinced\MonologScribeHandler;
 
 class ScribeHandler extends \Monolog\Handler\AbstractHandler
 {
+    private $scribe;
+
+    /**
+     * @param string $host the address of scribed
+     * @param integer $port the port of scribed
+     */
     public function __construct($host, $port)
     {
+        $this->scribe = new \Horde_Scribe_Client();
+        $this->scribe->connect($host, $port);
     }
 
     public function handle(array $record)
     {
+        $this->scribe->log($record['channel'], $record['message']);
     }
 }
