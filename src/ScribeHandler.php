b4d61dc727e417c410aeaae259f90b20de9c9d18
Plenty Su <plentysu@kkbox.com>
2014-12-06 01:53:37 +0800

    Add class skeleton: ScribeHandler

---
+++ src/ScribeHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iconvinced\MonologScribeHandler;
+
+class ScribeHandler extends \Monolog\Handler\AbstractHandler
+{
+    public function __construct($host, $port)
+    {
+    }
+
+    public function handle(array $record)
+    {
+    }
+}
